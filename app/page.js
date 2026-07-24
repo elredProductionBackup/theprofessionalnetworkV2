2d4183389b65c200f82f52fd85d59d56a5442578
MeezanOne <meezan@elred.io>
2026-07-24 11:38:26 +0530

    console added

--- app/page.js
+++ app/page.js
@@ -14,6 +14,7 @@ import ProfessionalsHero from "@/components/Tagline";
 import EventRegistration from "@/components/EventRegistration";
 
 export default function Home() {
+  console.log('Development')
   return (
     <main className="min-h-screen">
       <ProfessionalsHero />
